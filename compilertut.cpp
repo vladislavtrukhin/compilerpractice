3bc0d3977e70da67dae9a67c95fd0edc7d5d0c76
vladislavtrukhin <vladislavtrukhin@github.com>
2020-01-23 22:46:09 -0500

    Tutorial code for creating a compiler

---
+++ compilertut.cpp
@@ -0,0 +1,14 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 13
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+## %bb.0:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	$2, %eax
+	popq	%rbp
+	retq
+                                        ## -- End function
+
+.subsections_via_symbols
